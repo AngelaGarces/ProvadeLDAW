1c0babda42548214faadb6ae261c075d9e88dd15
AngelaGarces <angelacontapositivo@gmail.com>
2021-11-10 09:29:44 -0300

    Conectando as páginas para o site ficar funcional

--- projetodjango/urls.py
+++ projetodjango/urls.py
@@ -6,12 +6,12 @@ from apps.home import urls as home_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', include(home_urls)), #teste 09.11
-    path ('home/', include(home_urls)), 
-    path ('sobre/', include(home_urls)),
-    path ('penal/', include(home_urls)),
-    path ('civil/', include(home_urls)),
-    path ('militar/', include(home_urls)),
-    path ('bombeiros/', include(home_urls)),
-    path ('guarda/', include(home_urls)),
+    path('', include(home_urls)), #teste 09.11
+    path('home/', include(home_urls)), 
+    path('sobre/', include(home_urls)),
+    path('penal/', include(home_urls)),
+    path('civil/', include(home_urls)),
+    path('militar/', include(home_urls)),
+    path('bombeiros/', include(home_urls)),
+    path('guarda/', include(home_urls)),
 ]
