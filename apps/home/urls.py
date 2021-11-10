c38fa82f0a3243c17b94728b0989e82b86d4f379
AngelaGarces <angelacontapositivo@gmail.com>
2021-11-09 21:26:26 -0300

    Criando as demais páginas do site

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,9 +1,14 @@
 from django import urls
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, SobreView, MilitarView, CivilView, PenalView, BombeirosView, GuardaView
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-
     path('', HomeView.as_view(), name='Home'),
+    path('sobre/', SobreView.as_view(), name='Sobre'),
+    path('bombeiros/', BombeirosView.as_view(), name='Bombeiros'),
+    path('militar/', MilitarView.as_view(), name='Militar'),
+    path('civil/', CivilView.as_view(), name='Civil'),
+    path('guarda/', GuardaView.as_view(), name='Guarda'),
+    path('penal/', PenalView.as_view(), name='Penal'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -9,3 +9,64 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
 
         return context
+
+
+class SobreView(TemplateView):
+    template_name = "home/sobre.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super(SobreView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class CivilView(TemplateView):
+    template_name = "home/civil.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super(CivilView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class BombeirosView(TemplateView):
+    template_name = "home/bombeiros.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super(BombeirosView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class GuardaView(TemplateView):
+    template_name = "home/guarda.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super(GuardaView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class MilitarView(TemplateView):
+    template_name = "home/militar.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super(MilitarView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class PenalView(TemplateView):
+    template_name = "home/penal.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super(PenalView, self).get_context_data(**kwargs)
+
+        return context
+
