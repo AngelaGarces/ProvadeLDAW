c2daf76a4fc51bf92ae401011a2e7864f2d2f9af
AngelaGarces <angelacontapositivo@gmail.com>
2021-11-11 14:49:55 -0300

    pronta para configurar e pôr em deploy

--- projetodjango/settings.py
+++ projetodjango/settings.py
@@ -1,8 +1,6 @@
 import os
 
 from pathlib import Path
-from typing import cast
-from dj_database_url import config, parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,9 +55,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'projetodjango.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-DATABASES = {'default': config('DATABASE_URL', default = default_dburl, cast=dburl)}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
