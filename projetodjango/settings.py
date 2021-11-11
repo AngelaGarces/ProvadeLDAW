01099d9c284d86603f4724175a04dc2be9ac62e5
AngelaGarces <angelacontapositivo@gmail.com>
2021-11-11 20:42:27 -0300

    deploy

--- projetodjango/settings.py
+++ projetodjango/settings.py
@@ -1,6 +1,6 @@
 import os
 
-import django_heroku
+#import django_heroku
 
 from pathlib import Path
 
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,6 +110,7 @@ MEDIA_ROOT = 'media'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [ 
     'file_static'
@@ -119,4 +121,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
