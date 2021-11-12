812a356ca6d6de376bd84f4fc934bd1866807931
AngelaGarces <angelacontapositivo@gmail.com>
2021-11-12 09:07:49 -0300

    make it better

--- projetodjango/settings.py
+++ projetodjango/settings.py
@@ -62,14 +62,12 @@ WSGI_APPLICATION = 'projetodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = config('DATABASES')
-
-#  {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
